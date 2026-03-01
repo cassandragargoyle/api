1b96a47e17cb7639a124034143f1e4a2bdea31b0
zdendaku <zdendaku@gmail.cz>
2026-03-01 13:30:09 +0100

    Add EntityRepository interface to persistance module
    Generic CRUD repository interface for entity persistence operations.
    Eliminates empty JAR warning during build.

---
+++ persistance/src/main/java/org/cassandragargoyle/persistance/EntityRepository.java
@@ -0,0 +1,33 @@
+/*
+ *  This file is part of CassandraGargoyle Community Project
+ *  Licensed under the MIT License - see LICENSE file for details
+ */
+package org.cassandragargoyle.persistance;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Generic repository interface for entity persistence operations
+ *
+ * @param <T> the entity type
+ * @param <ID> the identifier type
+ * @author Zdenek
+ * @since 2026-03-01
+ */
+public interface EntityRepository<T, ID>
+{
+	Optional<T> findById(ID id);
+
+	List<T> findAll();
+
+	T save(T entity);
+
+	void delete(T entity);
+
+	void deleteById(ID id);
+
+	boolean existsById(ID id);
+
+	long count();
+}
